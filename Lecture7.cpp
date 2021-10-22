0a8b3faf7dc8afe28b7452693be1764e017c03c3
Krish Sapru <67235656+ksapru@users.noreply.github.com>
2021-10-22 15:20:38 -0400

    Update Lecture7.cpp

--- Lecture7.cpp
+++ Lecture7.cpp
@@ -2,7 +2,8 @@ L = 0
 R = "upper bound value"
 
 
-
+#include <bits/stdc++.h>
+using namespace std; 
 
 (Refer to the slides in the CS200 presentations)
 
@@ -26,10 +27,60 @@ int main() {
  while (l <= r) {
       int m = l + (r-1)/2;
       if (good(m)) {
-        r = m - 1 
+        l = m + 1;
+        
+      }
+      else {
+        r = m - 1; 
        }
+   
        // copy the remaining template
+
+   
+   
+   
+  // Copy Machine Problem 
+   
+f(T)
+  T -= min(x,y)
+  if (T < 0) return false;
+  else return (T/x + T/y+1) >= n 
+    
+
+ 
+#include <bits/stdc++.h>
+using namespace std; 
+
+bool good(int n) {
+  T -= min(x,y)
+  if (T < 0) return false;
+  else return (T/x + T/y+1) >= n 
+ }
+int n, x,y; 
+vector <int> v; 
+for (int i = 0; i < n; i++) {
+  int t; 
+  cin >> t; 
+  v.push_back(t);
+}
+
+int l = 0; 
+int r = 200:
+
+while (l <= r) {
+  int m = l + (r-1) / 2;
+  
+
+ 
+max(min()) problem 
+
+  
+L, R
+L = 0 
+R = INT_MAX; 
+Function 
        
+
 
 
 
